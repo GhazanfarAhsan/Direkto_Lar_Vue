8478cc673f333a6eddbc2e9eec6dd9f2061e3de7
caogiangnam523 <caogiangnam523@gmail.com>
2023-09-20 20:38:21 +0300

    Indicadores done

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,34 +1,25 @@
 <template>
-  <Breadcrumb
-  :paths="['Inicio']"
-   />
-  <div>Bienvenidos</div>
-  <slot></slot>
-
-  <ConfirmMultiple
-        v-if="modalName === 'ConfirmMultiple'"
-        :confirmHeader="''"
-        :header="'Invitaciones a Proyectos'"
-        :paragraphs="[
-          'Usted tiene actualmente las siguientes invitaciones confirme o rechace las invitaciones',
-        ]"
-        :infoProyectos = "infoProyectos"
-
-        @closeModal="closeModal"
-        @confirmChanges="confirmChanges"
-  />
+   <Breadcrumb
+      :paths="['Inicio', 'Analisis de restricciones', 'Indicadores']"
+      :urls ="['home']"
+      :settingFlag="false"
+      :class="status === 4 ? 'hidden' : 'm-1'"
+    />
+  <Dashboad />
 </template>
 
 <script>
 
 import ConfirmMultiple from "../components/ConfirmMultiple.vue";
+import Dashboad from "./Dashboard2.vue";
 import store from "../store";
 import Breadcrumb from "../components/Layout/Breadcrumb.vue";
 export default {
   name: "white-project-component",
   components: {
     Breadcrumb,
-    ConfirmMultiple
+    ConfirmMultiple,
+    Dashboad
   },
   data: function () {
     return {
@@ -36,7 +27,6 @@ export default {
               infoProyectos: [],
               firstLogueo  : 0
            }
-
   },
   methods: {
     handleRedirect(path) {
@@ -49,9 +39,7 @@ export default {
       if (this.modalName === "") this.$store.commit("increaseHint");
       else this.modalName = "";
     },
-
     confirmChanges: function (info) {
-
 
       store.dispatch('update_projects_without_approve', info).then(res => {
         console.log(res)
@@ -59,11 +47,8 @@ export default {
 
           this.closeModal();
           this.handleRedirect('proyectos')
-          //  this.infoProyectos = res.data.datos
         }
-
       });
-
     },
     getPendings : async function () {
       await store.dispatch('get_projects_without_approve').then(res => {
@@ -74,66 +59,15 @@ export default {
       });
 
     },
-
-
-
   },
   mounted:  async function() {
 
   //  store.dispatch('get_infoPerson');
   this.firstLogueo  = this.$store.state.user.firstLogin;
-  await this.getPendings();
-
   if(this.infoProyectos.length > 0 ){
       this.modalName = 'ConfirmMultiple'
       console.log(">>> entramos ConfirmMultiple")
    }
-
-  //   console.log(">>> comenzaoms el mounted")
-  // console.log(this.infoProyectos)
-
-
-
-  // console.log(">>> comenzaoms el mounted")
-  // console.log(this.infoProyectos)
-  //  if(this.firstLogueo == 1 && this.proyectoPendientes.length > 0 ){
-  //   this.modalName = 'ConfirmMultiple'
-  //   console.log(">>> entramos ConfirmMultiple")
-  //  }
-
   },
-  computed: {
-    // proyectoPendientes: function() {
-    //   return this.infoProyectos
-    // },
-
-    // primerLogueo: function(){
-
-    //   return this.$store.state.user.firstLogin;
-
-    // }
-  }
 }
-
-
-
-// import { computed, ref } from "vue";
-// import { useRoute } from "vue-router";
-// import { useStore } from "vuex";
-
-// const route = useRoute();
-// const store = useStore();
-// if (window.localStorage) {
-
-//   if (!localStorage.getItem('reload')) {
-//       localStorage['reload'] = true;
-//       window.location.reload();
-//   } else {
-//       localStorage.removeItem('reload');
-//   }
-// }
-/* store.dispatch('get_project')
-.catch((error) => {
-  console.log(error)
-}); */
 </script>
