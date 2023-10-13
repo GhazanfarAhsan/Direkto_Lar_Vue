05f5dc0d7f2ee42c4442d7e00f90448c6f12246a
caogiangnam523 <caogiangnam523@gmail.com>
2023-10-12 21:51:26 -0700

    API added

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/indicadores';
+    public const HOME = '/home';
 
     /**
      * The controller namespace for the application.
